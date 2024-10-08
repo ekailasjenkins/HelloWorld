2637fef8834fa6d89bf987ee305e6b599c095aa1
ekailasjenkins <ekailasjenkins@gmail.com>
2024-10-08 21:25:29 +0530

    Add Files

---
+++ src/newpackage/Variables.java
@@ -0,0 +1,15 @@
+package newpackage;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int age = 56;
+        byte age1 = 55;
+        int sadsa= 45;
+
+        System.out.println(age);
+
+
+    }
+}
